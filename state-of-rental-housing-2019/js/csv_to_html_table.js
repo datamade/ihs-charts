91f29b7ca7dc4d722e936855ed2752f755c85aa8
Derek Eder <derek.eder@gmail.com>
2019-04-20 13:55:56 -0500

    added table for chart 13

---
+++ state-of-rental-housing-2019/js/csv_to_html_table.js
@@ -0,0 +1,39 @@
+var CsvToHtmlTable = CsvToHtmlTable || {};
+
+CsvToHtmlTable = {
+    init: function (options) {
+        options = options || {};
+        var csv_path = options.csv_path || "";
+        var el = options.element || "table-container";
+        var csv_options = options.csv_options || {};
+        var datatables_options = options.datatables_options || {};
+
+        var $table = $("#" + el);
+
+        $.when($.get(csv_path)).then(
+            function (data) {
+                var csvData = $.csv.toArrays(data, csv_options);
+                var $tableBody = $("<tbody></tbody>");
+
+                for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
+                    var $tableBodyRow = $("<tr></tr>");
+                    $tableBodyRow.append("<td>" + csvData[rowIdx][0] + "</td>")
+                    for (var colIdx = 1; colIdx < csvData[rowIdx].length; colIdx++) {
+                        var cell_value = parseFloat(csvData[rowIdx][colIdx]);
+                        var style = 'danger';
+                        if (cell_value >= 0) {
+                            style = 'success';
+                            cell_value = "+" + cell_value;
+                        }
+                        var $tableBodyRowTd = $("<td class='" + style + "' style='text-align: right;'></td>");
+                        $tableBodyRowTd.text(cell_value + "%");
+                        $tableBodyRow.append($tableBodyRowTd);
+                        $tableBody.append($tableBodyRow);
+                    }
+                }
+                $table.append($tableBody);
+
+                $table.DataTable(datatables_options);
+            });
+    }
+};
